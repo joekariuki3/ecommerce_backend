c4e747ec52e0e997bd447e84423db57ad634fc69
joekariuki3 <joekariuki3@gmail.com>
2025-09-15 15:06:39 +0300

    feat: customized user model id to use uuid make email to be uniq also added string rep of user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.username}, {self.email}"
