10c19699f6e85af68d43fc9298f4cfedc9d842d0
joekariuki3 <joekariuki3@gmail.com>
2025-09-15 23:02:07 +0300

    feat: used default route to register api/users/me/ endpoint for user to view their information

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('me', UserViewSet)
 
 urlpatterns = [
+    *router.urls,
     path('register/', RegisterUserView.as_view(), name='register'),
 ]
