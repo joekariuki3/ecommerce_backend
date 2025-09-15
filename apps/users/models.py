a609a2fa8b0d94712ecafa18e716c713233a04ce
joekariuki3 <joekariuki3@gmail.com>
2025-09-15 16:27:32 +0300

    feat: on User model, made email to be used when loging in

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,5 +6,8 @@ class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     email = models.EmailField(unique=True)
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
     def __str__(self):
         return f"{self.username}, {self.email}"
