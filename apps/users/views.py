c60cc02a2ff8ddcebbeaf9d63ccc76f2d801770a
joekariuki3 <joekariuki3@gmail.com>
2025-09-15 15:09:56 +0300

    feat: added RegisterUserView to utilize the RegisterUserSerializer when registering a user using the post method

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterUserSerializer
 
-# Create your views here.
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
