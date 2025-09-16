d69e631ea91781853e52f7ca190fd76984029e68
joekariuki3 <joekariuki3@gmail.com>
2025-09-16 16:20:31 +0300

    feat: added rest_framework_simplejwt.token_blacklist in installed apps

--- core/settings/base.py
+++ core/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
 
+    "rest_framework_simplejwt.token_blacklist",
     # local apps
     'apps.users',
     'apps.catalog'
