1b2ed0927125e26bb8493e04d8e2bb5ebfd782e8
joekariuki3 <joekariuki3@gmail.com>
2025-09-16 16:23:17 +0300

    feat: add logout endpoint to authentication URLs

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from apps.users import urls as users_urls
+from apps.users.views import LogoutView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView
+    TokenVerifyView,
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/', include(users_urls, namespace='users')),
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("admin/", admin.site.urls),
+    path("api/users/", include(users_urls, namespace="users")),
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("api/auth/logout/", LogoutView.as_view(), name="logout"),
 ]
