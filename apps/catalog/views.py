59286c4f0675babedd3a3fc7fb0d7f704b82cde3
joekariuki3 <joekariuki3@gmail.com>
2025-09-17 14:18:31 +0300

    feat: implement CategoryViewSet for managing category instances

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CategorySerializer
+from .models import Category
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    """ A viewset for viewing and editing category instances. """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
