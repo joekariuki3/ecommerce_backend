fd8af4dabbd308a99e68b1461a884ebd7df2aa1f
joekariuki3 <joekariuki3@gmail.com>
2025-09-17 16:31:41 +0300

    feat: enhance views by adding ProductViewSet for product management

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import CategorySerializer
-from .models import Category
+from .serializers import CategorySerializer, ProductSerializer
+from .models import Category, Product
 from .permissions import IsAdminOrReadOnly
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
-    """ A viewset for viewing and editing category instances. """
+    """A viewset for viewing and editing category instances."""
+
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """A viewset for viewing and editing product instances."""
+
+    queryset = Product.objects.select_related("category").all()
+    serializer_class = ProductSerializer
     permission_classes = [IsAdminOrReadOnly]
