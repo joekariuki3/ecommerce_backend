4c57ffa54d32f4452a7eecc62760673f1222986e
joekariuki3 <joekariuki3@gmail.com>
2025-09-17 14:20:16 +0300

    feat: add category_factory fixture for creating Category instances in tests

--- conftest.py
+++ conftest.py
@@ -1,37 +1,57 @@
 import pytest
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
+from apps.catalog.models import Category
+
 
 @pytest.fixture
 def user_factory():
     """
     A factory fixture to create a User instance with optional keyword arguments.
     """
+
     def create_user(**kwargs):
         User = get_user_model()
         return User.objects.create_user(**kwargs)
+
     return create_user
+
 
 @pytest.fixture
 def default_user(user_factory):
     """
     Returns a simple, non-admin User instance.
     """
-    return user_factory(username="testuser", email="testuser@mail.com", password="password123")
+    return user_factory(
+        username="testuser", email="testuser@mail.com", password="password123"
+    )
+
 
 @pytest.fixture
 def admin_user(user_factory):
     """
     Returns a admin User instance.
     """
-    return user_factory(username="adminuser", email="adminuser@mail.com", password="password123", is_staff=True)
+    return user_factory(
+        username="adminuser",
+        email="adminuser@mail.com",
+        password="password123",
+        is_staff=True,
+    )
+
 
 @pytest.fixture
 def superuser(user_factory):
     """
     Returns a superuser instance.
     """
-    return user_factory(username="superuser", email="superuser@mail.com", password="password123", is_superuser=True)
+    return user_factory(
+        username="superuser",
+        email="superuser@mail.com",
+        password="password123",
+        is_superuser=True,
+    )
+
 
 @pytest.fixture
 def api_client():
@@ -39,6 +59,7 @@ def api_client():
     A test client instance.
     """
     return APIClient()
+
 
 @pytest.fixture
 def authenticated_client_and_user(default_user):
@@ -49,6 +70,7 @@ def authenticated_client_and_user(default_user):
     client.force_authenticate(user=default_user)
     return client, default_user
 
+
 @pytest.fixture
 def admin_authenticated_client(admin_user):
     """
@@ -57,3 +79,15 @@ def admin_authenticated_client(admin_user):
     client = APIClient()
     client.force_authenticate(user=admin_user)
     return client
+
+
+@pytest.fixture
+def category_factory():
+    """
+    A factory fixture to create Category instances with optional keyword arguments.
+    """
+
+    def create_category(**kwargs):
+        return Category.objects.create(**kwargs)
+
+    return create_category
