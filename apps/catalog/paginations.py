41588c4e315ddfaafa7da071e91cff0c94b2afd1
joekariuki3 <joekariuki3@gmail.com>
2025-09-18 17:05:13 +0300

    feat: ensure page_size is an integer and simplify page_size_query_param in ProductPagination

--- apps/catalog/paginations.py
+++ apps/catalog/paginations.py
@@ -4,6 +4,6 @@ import os
 
 class ProductPagination(PageNumberPagination):
     """Custom pagination class for products."""
-    page_size = os.getenv("PAGE_SIZE", 10)
-    page_size_query_param = os.getenv("PAGE_SIZE_QUERY_PARAM", "page_size")
+    page_size = int(os.getenv("PAGE_SIZE", 10))
+    page_size_query_param = "page_size"
     max_page_size = int(os.getenv("MAX_PAGE_SIZE", 100))
