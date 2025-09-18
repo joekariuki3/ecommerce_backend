5644d2497649e756eba14358743c25283771d57d
joekariuki3 <joekariuki3@gmail.com>
2025-09-18 11:58:16 +0300

    feat: update seed_users_db.py to use environment variables for admin and sample user creation

--- apps/users/management/commands/seed_users_db.py
+++ apps/users/management/commands/seed_users_db.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+import os
 
 class Command(BaseCommand):
     help = "Seed the database with sample users"
@@ -7,19 +8,25 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model()
         # Seed admin user
-        admin_email = "admin@example.com"
+        admin_email = os.environ.get("ADMIN_EMAIL")
         if not User.objects.filter(email=admin_email).exists():
             User.objects.create_superuser(
                 email=admin_email,
-                username="admin",
-                password="adminpass"
+                username=os.environ.get("ADMIN_USERNAME"),
+                password=os.environ.get("ADMIN_PASSWORD"),
             )
             self.stdout.write(self.style.SUCCESS("Admin user created."))
 
         # Seed sample users
         sample_users = [
-            {"email": "user1@example.com", "username": "user1", "password": "userpass1"},
-            {"email": "user2@example.com", "username": "user2", "password": "userpass2"},
+            {"email": os.environ.get("USER1_EMAIL"),
+             "username": os.environ.get("USER1_USERNAME"),
+             "password": os.environ.get("USER1_PASSWORD")
+             },
+            {"email": os.environ.get("USER2_EMAIL"),
+             "username": os.environ.get("USER2_USERNAME"),
+             "password": os.environ.get("USER2_PASSWORD")
+             },
         ]
         for u in sample_users:
             if not User.objects.filter(email=u["email"]).exists():
