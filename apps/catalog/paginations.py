72f96d3c260ed4c8cf7292b14a6d6ac49d6251c4
joekariuki3 <joekariuki3@gmail.com>
2025-09-18 16:25:11 +0300

    feat: ensure max_page_size is an integer in custom pagination

--- apps/catalog/paginations.py
+++ apps/catalog/paginations.py
@@ -6,4 +6,4 @@ class ProductPagination(PageNumberPagination):
     """Custom pagination class for products."""
     page_size = os.getenv("PAGE_SIZE", 10)
     page_size_query_param = os.getenv("PAGE_SIZE_QUERY_PARAM", "page_size")
-    max_page_size = os.getenv("MAX_PAGE_SIZE", 100)
+    max_page_size = int(os.getenv("MAX_PAGE_SIZE", 100))
