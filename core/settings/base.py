ed475249aafb08f43c50aa3b420e07d90a43f30f
joekariuki3 <joekariuki3@gmail.com>
2025-09-19 13:41:17 +0300

    feat: add drf_yasg to installed apps for API documentation support

--- core/settings/base.py
+++ core/settings/base.py
@@ -16,9 +16,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # third-party apps (installed via pip/uv)
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "django_filters",
+    'drf_yasg',
 
     # local apps
     "apps.users",
