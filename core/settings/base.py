e38e1ced9191131b1d04b4b70e3e2f54c5187838
joekariuki3 <joekariuki3@gmail.com>
2025-09-19 13:43:47 +0300

    feat: remove global  OrderingFilter and SearchFilter from default filter backends

--- core/settings/base.py
+++ core/settings/base.py
@@ -92,8 +92,6 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
-        "rest_framework.filters.OrderingFilter",
-        "rest_framework.filters.SearchFilter",
     ],
 }
 
