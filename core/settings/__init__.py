6859e16158d505c4e208011bfacc297948596a4f
joekariuki3 <joekariuki3@gmail.com>
2025-09-23 11:04:08 +0300

    feat: enhance environment configuration handling for clarity and maintainability

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,14 +1,23 @@
-import os
 from dotenv import load_dotenv
+from .base import * # noqa F401, F403
+import os
+
 load_dotenv()
 
 ENV = os.getenv("ENVIRONMENT")
 
 if ENV == "production":
-    from .production import *
-elif ENV == "staging":
-    from .staging import *
+    from .production import DEBUG, CSRF_COOKIE_SECURE, SESSION_COOKIE_SECURE, \
+        SECURE_BROWSER_XSS_FILTER, SECURE_CONTENT_TYPE_NOSNIFF, SECURE_SSL_REDIRECT
 elif ENV == "testing":
-    from .testing import *
+    from .testing import DATABASES
+elif ENV == "staging":
+    # No overrides needed, uses the defaults from base.py
+    pass
 else:
-    from .development import *
+    # Default to development environment
+    # No overrides needed, uses the defaults from base.py
+    pass
+
+__all__ = ["DATABASES", "DEBUG", "CSRF_COOKIE_SECURE", "SESSION_COOKIE_SECURE",
+           "SECURE_BROWSER_XSS_FILTER", "SECURE_CONTENT_TYPE_NOSNIFF", "SECURE_SSL_REDIRECT"]
