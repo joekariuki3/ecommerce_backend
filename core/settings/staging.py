e837a27fca8b366578948a559848d439f710902e
joekariuki3 <joekariuki3@gmail.com>
2025-09-23 11:03:46 +0300

    feat: simplify development and staging configurations by removing overrides

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -1,18 +1,2 @@
-from .base import *
-import os
-
-print(f"ENV: Staging Environment being used")
-
-
-DEBUG = os.getenv('DEBUG')
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
-}
+# No overrides needed, uses the defaults from base.py
+pass
